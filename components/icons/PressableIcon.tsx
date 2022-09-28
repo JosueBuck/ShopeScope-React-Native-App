aca65e19bd618a2b3333def6f40c6c2ab8e3458a
JosueBuck <buck.josue@gmx.de>
2022-09-28 07:06:48 -0500

    Add custom styling to pressable icon component

--- components/icons/PressableIcon.tsx
+++ components/icons/PressableIcon.tsx
@@ -1,4 +1,4 @@
-import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View, ViewStyle } from 'react-native';
 import React from 'react';
 import IonIconIcon from './IonIconIcon';
 
@@ -6,12 +6,13 @@ type Props = {
     func: any,
     iconName: string,
     iconSize: number,
-    iconColor: string
+    iconColor: string,
+    customStyling?: ViewStyle | ViewStyle[]
 }
 
-const PressableIcon: React.FC<Props> = ({func, iconName, iconSize, iconColor}) => {
+const PressableIcon: React.FC<Props> = ({func, iconName, iconSize, iconColor, customStyling}) => {
     return (
-    <Pressable onPress={func}>
+    <Pressable style={customStyling} onPress={func}>
         <IonIconIcon name={iconName} size={iconSize} color={iconColor} />
     </Pressable>
   )
