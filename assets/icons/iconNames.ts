bd011e7003ef7db72dca145484276c550564d1b6
JosueBuck <buck.josue@gmx.de>
2022-09-28 07:03:36 -0500

    Add icon name file

--- assets/icons/iconNames.ts
+++ assets/icons/iconNames.ts
@@ -1,3 +1,8 @@
 export enum IconName {
-    PROFILE = 'person-circle-sharp'
+    PROFILE = 'person-circle-sharp',
+    REMOVE = 'remove-circle-outline',
+    ADD = 'ios-add-circle-outline',
+    CLOSE = 'ios-close-circle-outline',
+    TRASH = 'ios-trash-outline',
+    SEND = 'ios-send-outline'
 }
