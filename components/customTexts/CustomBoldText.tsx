e43d58a4de3f9a722a2f45fc9748b31b1d44e52f
JosueBuck <buck.josue@gmx.de>
2022-09-28 07:06:29 -0500

    Adjust custome text components - fix custome bold spelling - adjust color imports - add custom styling to medium text

--- components/customTexts/CustomBoldText.tsx
+++ components/customTexts/CustomBoldText.tsx
@@ -1,19 +1,19 @@
 import { StyleSheet, Text } from 'react-native';
 import React from 'react';
-import { colors } from '../../assets/colors/colors';
+import { colors } from '../../assets/globalStyling/colors';
 
 type Props = {
     children: React.ReactNode,
     fontSize?: number
 }
 
-const CustomLBoldText: React.FC<Props> = ({children, fontSize}) => {
+const CustomBoldText: React.FC<Props> = ({children, fontSize}) => {
   return (
     <Text style={[{fontSize: fontSize}, styles.text]}>{children}</Text>
   )
 }
 
-export default CustomLBoldText;
+export default CustomBoldText;
 
 const styles = StyleSheet.create({
     text: {
