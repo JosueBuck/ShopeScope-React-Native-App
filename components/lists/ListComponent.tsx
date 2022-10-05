233c168b976efec5220d75810aaaf43e2ec92095
JosueBuck <buck.josue@gmx.de>
2022-10-04 21:02:56 -0500

    Fix list component -add scrollview to list name

--- components/lists/ListComponent.tsx
+++ components/lists/ListComponent.tsx
@@ -1,4 +1,4 @@
-import { ImageBackground, Pressable, StyleSheet, View, ViewStyle } from 'react-native';
+import { ImageBackground, Pressable, ScrollView, StyleSheet, View, ViewStyle } from 'react-native';
 import React from 'react';
 import { IList, ISimplifiedList } from '../../models/lists.model';
 import { basicShadow } from '../../assets/globalStyling/shadows';
@@ -14,15 +14,19 @@ type Props = {
 
 const ListComponent: React.FC<Props> = ({ navigation, listData, strechable, customContainerStyling }) => {
     return (
-        <Pressable onPress={() => navigation.navigate('Content', {screen: 'ListsOverview'})} style={[styles.listContainer, basicShadow, strechable ? styles.strechable : styles.fix, customContainerStyling]}>
-            <ImageBackground imageStyle={styles.image} style={ strechable ? styles.strechableImageContainer : styles.fixImageContainer} source={{ uri: listData.listPictureUrl }} />
+        <View style={[styles.listContainer, basicShadow, strechable ? styles.strechable : styles.fix, customContainerStyling]}>
+            <ImageBackground imageStyle={styles.image} style={strechable ? styles.strechableImageContainer : styles.fixImageContainer} source={{ uri: listData.listPictureUrl }} />
             <View style={styles.listInfoContainer}>
-                <CustomMediumText>{listData.name}</CustomMediumText>
+                <ScrollView horizontal={true} style={styles.listNameContainer} showsHorizontalScrollIndicator={false}>
+                    <Pressable onPress={() => navigation.navigate('Content', { screen: 'ListsOverview' })} >
+                        <CustomMediumText>{listData.name}</CustomMediumText>
+                    </Pressable>
+                </ScrollView>
                 <View style={[styles.itemCounterContainer, basicShadow]}>
                     <CustomMediumText>{listData.itemCounter}</CustomMediumText>
                 </View>
             </View>
-        </Pressable>
+        </View>
     )
 }
 
@@ -47,7 +51,7 @@ const styles = StyleSheet.create({
         height: 140
     },
     fixImageContainer: {
-        height: 160
+        height: 160,
     },
     image: {
         borderRadius: 15
@@ -60,10 +64,13 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         paddingHorizontal: 20
     },
+    listNameContainer: {
+        marginRight: 10
+    },
     itemCounterContainer: {
         width: 25,
         height: 25,
-        borderRadius: 12.5, 
+        borderRadius: 12.5,
         backgroundColor: colors.white,
         justifyContent: 'center',
         alignItems: 'center'
